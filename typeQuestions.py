a3d1737590a2160ce5bb4966ff95e9a32d356a94
Trevor Dalton <trevorjd3141@gmail.com>
2020-11-05 02:28:24 -0700

    Place Question functions inside class. Define type and subtype.

--- typeQuestions.py
+++ typeQuestions.py
@@ -5,19 +5,68 @@ Who, What, Why, Where, When, How
 """
 import os
 
-def cleanline(line):
-    return line.split(':')[1].strip()
-
-def containsSubstrings(line, matches):
-    return any(value in line.lower() for value in matches)
-
 class Question:
 
     def __init__(self, id, text, difficulty):
-        self.id = cleanline(id)
-        self.text = cleanline(text)
-        self.difficulty = cleanline(difficulty)
+        self.id = self.cleanline(id)
+        self.text = self.cleanline(text)
+        self.difficulty = self.cleanline(difficulty)
 
+        self.type = ''
+        potentialTypes = (['Who'], ['What', 'Which'], ['Why'], ['When', 'What Year','How Far Back', 'How Long Ago'],
+                            ['Where'], ['How'], ['Quantity', 'How Much', 'How Many', 'How Far'])
+        for i in range(len(potentialTypes)):
+            if self.containsSubstrings(text, potentialTypes[i], True):
+                self.type = potentialTypes[i][0]
+                break
+
+        if self.type == '':
+            self.type = 'What'
+
+        self.subtype = ''
+        self.defineSubtype()
+
+        print(self.id, self.text, self.difficulty, self.type, self.subtype)
+    
+    def cleanline(self, line):
+        return line.split(':')[1].strip()
+
+    # Add the extra spaces to avoid cases such as 'age' in
+    # 'teenage' triggering a true value
+    def containsSubstrings(self, line, matches, lower=False):
+        line += ' '
+        if lower:
+            return any(' ' + value.lower() + ' ' in line.lower() for value in matches)
+        else:
+            return any(' ' + value + ' ' in line.lower() for value in matches)
+
+    def defineSubtype(self):
+        if self.containsSubstrings(self.text, ['time', 'hour', 'How long']) and self.type in ('When', 'Quantity'):
+            self.subtype = 'time'
+        elif self.containsSubstrings(self.text, ['date', 'year']) and self.type in ('When'):
+            self.subtype = 'date'
+        elif self.containsSubstrings(self.text, ['age', 'old']) and self.type in ('Quantity'):
+            self.subtype = 'age'
+        elif self.containsSubstrings(self.text, ['cost', 'price', 'dollar']) and self.type in ('Quantity', 'What'):
+            self.subtype = 'price'
+        elif self.containsSubstrings(self.text, ['tall', 'distance', 'tall', 'wide', 'deep']) and self.type in ('Quantity', 'How'):
+            self.subtype = 'length'
+        elif self.containsSubstrings(self.text, ['weigh', 'heavy']) and self.type in ('Quantity'):
+            self.subtype = 'weight'
+        elif self.containsSubstrings(self.text, ['Who is']) and len(self.text) == 4:
+            self.subtype = 'simple who'
+        elif self.containsSubstrings(self.text, ['What is']) and 5 > len(self.text) > 2:
+            self.subtype = 'simple what'
+        elif self.containsSubstrings(self.text, ['country', 'nation']) and self.type == 'What':
+            self.subtype = 'country'
+        elif self.containsSubstrings(self.text, ['province', 'territory']) and self.type == 'What':
+            self.subtype = 'province'
+        elif self.containsSubstrings(self.text, ['state', 'governor', 'states']) and self.type == 'What':
+            self.subtype = 'state'
+        elif self.containsSubstrings(self.text, ['mayor', 'city', 'town', 'village']) and self.type == 'What':
+            self.subtype = 'city'
+        elif self.containsSubstrings(self.text, ['type', 'what kind', 'which kind']) and self.type == 'What':
+            self.subtype = 'class'
 
 def getQuestionFilenames():
     cwd = os.getcwd()
