49d9846447682ca3aad43446ca340720bb75b90d
Spencer <bondsspencer@gmail.com>
2020-11-05 22:09:08 -0700

    added outline

--- qa.py
+++ qa.py
@@ -11,6 +11,7 @@ def classify(question, token):
 def filterTokens(storyID):
     # Grabs all questions that pertain to the 
     # story in question and sorts them
+
     pass
 
 def filterQuestions(storyID):
@@ -22,7 +23,18 @@ def output(id, answer):
     # Formats an id and answer to what we need
     # QuestionID: ...
     # Answer: ...
-    #
+    # !! If just need to print:
+    print('QuestionID: ' + id + '\n')
+    # !! NOTE TO SELF:  Do we need to double return on end of answer??
+    print('Answer: ' + answer + '\n')
+
+    # # !! If we have to write to a file we'll need some 3rd file paramater
+    # # !! (AND will need to OPEN and CLOSE it in qa())
+    # # !! Same applies here about double returns after answer
+    # # !! NOTE: If we do write to some file it will be in project folder (no return)
+    # file.write('QuestionID: ' + id  + '\n'
+    #            + 'Answer: ' + answer + '\n')
+
     pass
 
 def qa():
@@ -40,18 +52,24 @@ def qa():
     fileLines = inputFile.readlines()
 
     # for each storyID retrieve the NRE and appropriate questions
-    for ID in fileLines:
+    for id in fileLines:
         # !! not sure if better way to skip first in for-each, could set to null?
-        if fileLines[0] == ID:
+        if fileLines[0] == id:
             continue
         # !! Assured we're only catching IDs, we continue
         # !! NOTE TO SELF: check if values need to be returned/stored for these calls
-        filteredQuestions = filterQuestions(ID)
-        filteredTokens = filterTokens(ID)
+        filteredQuestions = filterQuestions(id)
+        filteredTokens = filterTokens(id)
 
         # Sort questions and go over each in its time applying appropriate rules
         # !! Not sure if above just means to pass to classifier
-        classify(filteredQuestions, filteredTokens)
+        ans = classify(filteredQuestions, filteredTokens)
+
+        # Output ID with answer
+        output(id, ans)
+
+    # !! Close file
+    inputFile.close()
 
     pass
 
