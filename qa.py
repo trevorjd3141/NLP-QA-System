2a2fa913f80932cc9671c98067adb8badd043b75
Spencer <bondsspencer@gmail.com>
2020-11-06 19:33:47 -0700

    subtypes filled out

--- qa.py
+++ qa.py
@@ -14,6 +14,7 @@ def classify(question, story):
     for token in tokens:
         if token.ent_ in potentialEntities:
             potentialAnswers.append(token.text)
+    print('made it here')
 
     return random.choice(potentialAnswers)
 
@@ -57,6 +58,38 @@ def qa():
     file.close()
     pass
 
+    # self.subtype = ''
+    #     if self.containsSubstrings(self.text, ['time', 'hour', 'How long']) and self.type in ('When', 'Quantity'):
+    #         self.subtype = 'time'
+    #     elif self.containsSubstrings(self.text, ['date', 'year']) and self.type in ('When'):
+    #         self.subtype = 'date'
+    #     elif self.containsSubstrings(self.text, ['age', 'old']) and self.type in ('Quantity'):
+    #         self.subtype = 'age'
+    #     elif self.containsSubstrings(self.text, ['cost', 'price', 'dollar']) and self.type in ('Quantity', 'What'):
+    #         self.subtype = 'price'
+    #     elif self.containsSubstrings(self.text, ['tall', 'distance', 'tall', 'wide', 'deep']) and self.type in ('Quantity', 'How'):
+    #         self.subtype = 'length'
+    #     elif self.containsSubstrings(self.text, ['weigh', 'heavy']) and self.type in ('Quantity'):
+    #         self.subtype = 'weight'
+    #     elif self.containsSubstrings(self.text, ['Who is']) and 6 > len(self.text.split()) > 2:
+    #         self.subtype = 'simple who'
+    #     elif self.containsSubstrings(self.text, ['What is']) and 6 > len(self.text.split()) > 2:
+    #         self.subtype = 'simple what'
+    #     elif self.containsSubstrings(self.text, ['Where is']) and 6 > len(self.text.split()) > 2:
+    #         self.subtype = 'simple where'
+    #     elif self.containsSubstrings(self.text, ['country', 'nation']) and self.type == 'What':
+    #         self.subtype = 'country'
+    #     elif self.containsSubstrings(self.text, ['province', 'territory']) and self.type == 'What':
+    #         self.subtype = 'province'
+    #     elif self.containsSubstrings(self.text, ['state', 'governor', 'states']) and self.type == 'What':
+    #         self.subtype = 'state'
+    #     elif self.containsSubstrings(self.text, ['mayor', 'city', 'town', 'village']) and self.type == 'What':
+    #         self.subtype = 'city'
+    #     elif self.containsSubstrings(self.text, ['type', 'what kind', 'which kind']) and self.type == 'What':
+    #         self.subtype = 'class'
+    #     elif self.containsSubstrings(self.text, ['work', 'volunteer']) and self.type == 'Where':
+    #         self.subtype = 'organization'
+
 # Return valid entity types given the question type
 def questionMatchEntity(question):
     if question.type == 'Who':
@@ -65,14 +98,30 @@ def questionMatchEntity(question):
         # ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LAW', 'LANGUAGE', 'DATE', 'TIME', 'PERCENTAGE', 'ORDINAL']
         return []
     elif question.type == 'When':
+        if question.subtype == 'time':
+            return ['TIME']
+        elif question.subtype == 'date':
+            return ['DATE']
         return ['DATE', 'TIME', 'PERCENT', 'ORDINAL']
     elif question.type == 'Where':
+        if question.subtype == 'organization':
+            return ['ORG']
         return ['FAC', 'ORG', 'GPE', 'LOC', 'EVENT']
     elif question.type == 'Why':
         return []
     elif question.type == 'How':
         return []
     elif question.type == 'Quantity':
+        if question.subtype == 'time':
+            return ['TIME']
+        elif question.subtype == 'age':
+            return ['DATE', 'TIME']
+        elif question.subtype == 'price':
+            return ['PERCENT', 'MONEY']
+        elif question.subtype == 'length':
+            return ['QUANTITY']
+        elif question.subtype == 'weight':
+            return ['QUANTITY']
         return ['DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
     else:
         return []
